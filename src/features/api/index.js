df127c113eaa1d093f265210ac5751ba8536686e
Randy Casburn <randycasburn@users.noreply.github.com>
2023-02-21 13:56:18 -0600

    environment

--- src/features/api/index.js
+++ src/features/api/index.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3030/api/bookreactions/";
+const url = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_DEV_API_URL;
 
 const headers = {
     accept: 'application/json',
